6ea087f18e5da067d4c992020add6b35241d0f3c
Victor Boone <victor.boone@ens-lyon.fr>
2018-02-13 09:40:08 +0100

    boone--pauvert : first commit

---
+++ boone--pauvert/auto_tests.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+tests_dir="tests/"
+sol_dir="solutions/"
+
+
+make
+
+
+echo "\n **************** Starting Tests *****************"
+for test in $(ls $tests_dir)
+do
+	printf "Testing %-10s : %10s" $test
+	res=$(./main.native < $tests_dir/$test)
+	printf "%10s" $res
+	if [ -e $sol_dir/$test ]
+	then
+		solution=$(cat $sol_dir/$test)
+		if [ "-"$solution = "-"$res ]
+		then
+			echo "\t\t--Right Answer"
+		else
+			echo -n "\t\t--Wrong Answer"
+			printf " - should be \"%s\"\n" $solution
+		fi
+	else
+		echo "\t\tERROR : $sol_dir/$test"
+	fi
+done;
